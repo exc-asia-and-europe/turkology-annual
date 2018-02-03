48113219732c35a1b1d1226c6599255aa077f3e6
Dustin Heckmann <dustin-heckmann@users.noreply.github.com>
2018-02-03 18:15:57 +0100

    Rewrite WML parser using lxml, extract style info

--- src/paragraph/wmlparser.py
+++ src/paragraph/wmlparser.py
@@ -1,159 +1,111 @@
-"""
-This module includes the WMLParser class.
-
-It was developed as a contribution to the project Turkology Annual Online
-at the Cluster of Excellence "Asia and Europe" at the University of Heidelberg. 
-"""
-
-
-import pickle
-from xml.dom.minidom import *
-
-from paragraph.wmltable import WMLTbl
-
-from paragraph.wmlparagraph import WMLParagraph
+from collections import OrderedDict
+from lxml import etree
 
 
 class WMLParser(object):
-    '''
-    This class includes methods for extracting information from WML-Files.
-    '''
-    domWordDocument = None
-    domBody = None
-    #domLists = None # Not used (DH)
-    domStyles = None
-    paragraphs = []
-    tbls = []
-    styleToSmallCaps = {}
-    outputFileName = ''
-    
-    def extractBody(self):
-        '''
-        This method extracts the <w:body> node from the WML-Document.
-        '''
-        for child in self.domWordDocument.childNodes:
-            if child.nodeName=='w:body':
-                return child
-        else:
-            raise Exception('Body not found')
+    def __init__(self, xmlFileName):
+        self.namespaces = {
+            'w': 'http://schemas.microsoft.com/office/word/2003/wordml',
+            'wx': 'http://schemas.microsoft.com/office/word/2003/auxHint',
+        }
+        self.document = self._parse_document(xmlFileName)
+        self.styles = self._extract_char_styles()
+        self.paragraphs = self._extract_paragraphs()
 
-    def extractLists(self):
-        '''
-        This method extracts the <w:list> node from the WML-Document
-        '''
-        result = None
-        for child in self.domWordDocument.childNodes:
-            if child.nodeName=='w:lists':
-                result = child
-        return result  
-    
-    def extractStyles(self):
-        '''
-        This method extracts the <w:styles> node from the WML-Document
-        '''
-        result = None
-        for child in self.domWordDocument.childNodes:
-            if child.nodeName=='w:styles':
-                result = child
-        return result 
+    def _parse_document(self, xml_filename):
+        return etree.parse(xml_filename).getroot()
 
-    def extractParagraphs(self):
-        '''
-        This method extracts particular paragraphs from the elements of 
-        self.tbls list. c.f. def extractTbls for reference.
-        '''
-        result = []
-        for child in self.domBody.getElementsByTagName('wx:sect')[0].childNodes:
-            if child.nodeName=='w:tbl':
-                table = WMLTbl(child, parser=self)
-                for paragraph in table.paragraphs:
-                    yield paragraph
-            elif child.nodeName == 'w:p':
-                yield WMLParagraph(child)
 
-        '''for tbl in self.tbls:
-            for paragraph in tbl.paragraphs:
-                #print(paragraph.domPTag.toxml())
-                result.append(paragraph)
-            #result.extend(tbl.paragraphs)
-        return result'''
-    
-    def extractTbls(self):
-        '''
-        This method extracts the <w:tbl> nodes from the WML-Document,
-        for each found <w:tbl> it creates the corresponding WMLTbl instance,
-        and returns a list containing them.
-        '''
-        result = []
-        idx = 0
-        for child in self.domBody.getElementsByTagName('wx:sect')[0].childNodes:
-            if child.nodeName=='w:tbl':
-                result.append(WMLTbl(child, idx, self))
-                idx+=1
-        return result
-    
+    ''' <wx:font wx:val="Arial Unicode MS"></wx:font>
+      <w:sz w:val="16"></w:sz>
+      <w:sz-cs w:val="16"></w:sz-cs>
+      <w:b w:val="off"></w:b>
+      <w:b-cs w:val="off"></w:b-cs>
+      <w:i w:val="off"></w:i>
+      <w:i-cs w:val="off"></w:i-cs>
+      <w:smallCaps w:val="off"></w:smallCaps>'''
+
+    def _extract_char_styles(self):
+        char_styles = OrderedDict()
+        for style_node in self._xpath('/w:styles/w:style[@w:type=\'character\']'):
+            char_style = {}
+            style_id = self._xpath('./@w:styleId', element=style_node)[0]
+            for attribute_node in self._xpath('w:rPr/*[not(@w:val)]|w:rPr/*[@w:val!=\'off\']', element=style_node):
+                value = attribute_node.get('{%s}%s' % (self.namespaces['wx'], 'val')) or attribute_node.get('{%s}%s' % (self.namespaces['w'], 'val'))
+                if value:
+                    if value == 'on':
+                        value = True
+                    elif value.isdigit():
+                        value = int(value)
+                else:
+                    value = True
+                attribute_name = attribute_node.tag.split('}')[1]
+                char_style[attribute_name] = value
+            char_styles[style_id] = char_style
+        return char_styles
+
+    def _extract_paragraphs(self):
+        paragraph_nodes = self._xpath('//w:p')
+        for paragraph_index, paragraph_node in enumerate(paragraph_nodes):
+            paragraph = {
+                'index': paragraph_index,
+            }
+            paragraph.update(self._get_paragraph_text_and_styles(paragraph_node))
+
+            yield paragraph
+
+    def _get_paragraph_text_and_styles(self, paragraph_node):
+        text_parts = []
+        index_start = 0
+        style_ranges = OrderedDict()
+        previous_style_id = None
+        for text_node in self._xpath('w:r/w:t', element=paragraph_node):
+            text_part = text_node.text
+            if text_part is None:
+                continue
+            text_parts.append(text_part)
+            index_end = index_start + len(text_part)
+            style_id = self._xpath('w:rPr/w:rStyle/@w:val', element=text_node.getparent())
+            if style_id:
+                style_id = style_id[0]
+                if style_id == previous_style_id:
+                    (last_index_start, _), last_style = style_ranges.popitem(last=True)
+                    style_ranges[(last_index_start, index_end)] = last_style
+                else:
+                    style_ranges[(index_start, index_end)] = self.styles[style_id]
+            previous_style_id = style_id
+            index_start = index_end
+            #print(tTag)
+        return {
+            'text': ''.join(text_parts),
+            'styles': style_ranges,
+        }
+        #pPr_node = self._xpath('w:pPr', element=paragraph_node)[0]
+
+
     def extractStyleToSmallCaps(self):
         '''
         This method extracts information on small caps from the document and 
         saves the information about indexes where small caps start and end into
         a dictionary. This mapping is then returned.
         '''
-        result= {}
-#        print(self.domStyles.childNodes)
-        for child in self.domStyles.childNodes:
-            if child.getAttribute('w:type')=='character':
-                styleId = child.getAttribute('w:styleId')
+        result = {}
+        #        print(self.domStyles)
+        for child in self.styles:
+            if child.get('w:type') == 'character':
+                styleId = child.get('w:styleId')
                 smallCapsTags = child.getElementsByTagName('w:smallCaps')
-                if len(smallCapsTags)>0:
-                    if smallCapsTags[0].getAttribute('w:val')=='on':
-                        result.setdefault(styleId, True)
+                if smallCapsTags:
+                    if smallCapsTags[0].get('w:val') == 'on':
+                        result[styleId] = True
                     else:
-                        result.setdefault(styleId, False)
+                        result[styleId] = False
                 else:
-                    result.setdefault(styleId, False)
+                    result[styleId] = False
         return result
-               
-    
-    def getTxtOutput(self, outputFileName=None):
-        '''
-        This method return the string output of the whole document. The output is 
-        formatted as FormattedString, which enables to use specific methods for this type.
-        '''
-        result= ''
-        for par in self.paragraphs:
-            result = result + par.getTextAsFormattedString() + '\n'
-        if outputFileName is not None:
-            self.outputFileName = outputFileName
-            resultFileName = self.outputFileName
-            resultFile = open(resultFileName, 'wb')
-            pickle.dump(result, resultFile)
-            resultFile.close
-#        else:
-#            self.outputFile ='../../output-txt/test-probe-table' + datetime.today().isoformat().replace(':', '-') +'.txt'
-        return result
-                            
 
-    def __init__(self, xmlFileName):
-        '''
-        Constructor
-        '''
-        try:
-            self.domWordDocument = parse(xmlFileName).documentElement
-            #self.domLists = self.extractLists()  # Not used (DH)
-            self.domStyles = self.extractStyles()
-            self.styleToSmallCaps = self.extractStyleToSmallCaps()
-            self.domBody = self.extractBody()
-            self.tbls = self.extractTbls()
-            self.paragraphs = self.extractParagraphs()
-#            self.getTxtOutput()
-#            self.assignRecordToParagraph()
-#            self.printParagraphs()
-#            print(self.paragraphDict)
-        except Exception:
-            print ('Details: \n')
-            raise
-        
-if __name__ == '__main__':
-    '''
-    '''
-    wordml = WMLParser('../../input/TA19.xml') 
+    def _xpath(self, expression, element=None):
+        element = element or self.document
+        if element is self.document:
+            expression = '/w:wordDocument' + ('' if expression[0] == '/' else '/') + expression
+        return element.xpath(expression, namespaces=self.namespaces)
